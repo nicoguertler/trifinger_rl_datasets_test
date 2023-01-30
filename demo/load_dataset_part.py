bb5e6e0e2839f93e00ad59d94ffe636d0af90aa4
Nico Gürtler <nico.guertler@tuebingen.mpg.de>
2023-01-30 10:13:20 +0100

    Add arguments to demo that loads a part of a dataset.

--- demo/load_dataset_part.py
+++ demo/load_dataset_part.py
@@ -1,19 +1,43 @@
 """Load part of a datset defined by a range of transitions."""
 
 
+import argparse
+
 import gymnasium as gym
 
 import trifinger_rl_datasets  # noqa
 
 
 if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description=__doc__)
+    argparser.add_argument(
+        "--env",
+        type=str,
+        default="trifinger-cube-push-real-expert-image-mini-v0",
+        help="Name of dataset environment to load.",
+    )
+    argparser.add_argument(
+        "--range",
+        type=int,
+        nargs=2,
+        default=[1000, 2000],
+        help="Range of timesteps to load image data for.",
+    )
+    argparser.add_argument(
+        "--h5path",
+        type=str,
+        default=None,
+        help="Path to HDF5 file to load.",
+    )
+    args = argparser.parse_args()
+
     env = gym.make(
-        "trifinger-cube-push-real-expert-image-mini-v0",
+        args.env,
         disable_env_checker=True
     )
 
     # load only a subset of obervations, actions and rewards
-    dataset = env.get_dataset(rng=(1000, 2000))
+    dataset = env.get_dataset(rng=tuple(args.range), h5path=args.h5path)
 
     n_observations = len(dataset["observations"])
     print("Number of observations: ", n_observations)
