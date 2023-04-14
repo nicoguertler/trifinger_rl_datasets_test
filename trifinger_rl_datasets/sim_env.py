9a20c888cb6014ed98ba9778c93a13b6e5c849ef
Nico Gürtler <nico.guertler@tuebingen.mpg.de>
2023-04-14 15:56:44 +0200

    Check if images are Bayer encoded and decode them if so.

--- trifinger_rl_datasets/sim_env.py
+++ trifinger_rl_datasets/sim_env.py
@@ -3,6 +3,7 @@ from time import sleep, time
 from typing import Tuple, Dict, Any, Optional
 import logging
 
+import cv2
 import gymnasium as gym
 import numpy as np
 from pybullet import ER_TINY_RENDERER
@@ -566,11 +567,13 @@ class SimTriFingerCubeEnv(gym.Env):
             "confidence": np.array([object_observation.confidence], dtype=np.float32),
         }
         if self.image_obs:
-            # RGB camera images created with software renderer
-            # (using openGL requires GUI to run)
-            images = np.array([cam.image for cam in camera_observation.cameras])
-            # print(camera_observation.cameras[0].image.shape) # TODO: remove
-            # print(images.shape)
+            if len(camera_observation.cameras[0].image.shape) == 2:
+                # images from real platform have to be debayered
+                images = np.array([cv2.cvtColor(cam.image, cv2.COLOR_BAYER_BG2RGB) for cam in camera_observation.cameras])
+            else:
+                # RGB camera images created with software renderer
+                # (using openGL requires GUI to run)
+                images = np.array([cam.image for cam in camera_observation.cameras])
             # convert to channel first
             images = np.transpose(images, (0, 3, 1, 2))
             camera_obs_processed["images"] = images
